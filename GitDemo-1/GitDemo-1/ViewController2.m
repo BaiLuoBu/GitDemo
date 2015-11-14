9111b6b09441ff1d31bab32e93d6f0e167d66199
mgshan <757136771@qq.com>
2015-11-14 22:24:48 +0800

    B commit

---
+++ GitDemo-1/GitDemo-1/ViewController2.m
@@ -0,0 +1,37 @@
+//
+//  ViewController2.m
+//  GitDemo-1
+//
+//  Created by qianfeng01 on 15/11/14.
+//  Copyright (c) 2015年 MaGS. All rights reserved.
+//
+
+#import "ViewController2.h"
+
+@interface ViewController2 ()
+
+@end
+
+@implementation ViewController2
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
